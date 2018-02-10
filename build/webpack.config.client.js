6321757dee6174b7f42a1f553f92dd2e49586ca0
suifengfengye <1527261080@qq.com>
2018-02-11 00:01:42 +0800

    client webpack config

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -3,12 +3,12 @@ const path = require('path');
 const config = {
     entry: {
         // app: [path.resolve(__dirname, '../src/entry-client.js')]
-        app: ['../src/entry-client.js']
+        app: ['./src/entry-client.js']
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: '[name].[hashchunk:6].js',
-        chunkName: '[name].[hashchunk:6].[ext]'
+        filename: '[name].[chunkhash:6].js',
+        chunkFilename: '[name].[chunkhash:6].[ext]'
     },
     module: {
         rules: [
@@ -16,6 +16,16 @@ const config = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                      less: 'vue-style-loader!css-loader!less-loader', // <style lang="less">
+                    }
+                }
             }
         ]
     }
