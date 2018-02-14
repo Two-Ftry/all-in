9b6d323043459d07dc16eef86bcf28b34392d45c
suifengfengye <1527261080@qq.com>
2018-02-14 16:31:23 +0800

    xx1

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 <div id="app">
   <div class="root-box">
       my ssr App.vue@@@
+      <transition name="fade" mode="out-in">
+        <router-view class="view"></router-view>
+      </transition>
   </div>
 </div>
 </template>
